fcc822a61fbe9b0b286548c7e0e7b7baa4ad339b
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-30 11:56:38 +0100

    aggiunta versione precedente commentata nel foglio JS

--- main.js
+++ main.js
@@ -60,3 +60,35 @@ fetch(endpoint)
   .catch((error) => {
     console.log("Errore nel recupero dei dati:", error);
   });
+
+/////////Versione precedente con classlist create element e appendchild//////
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const board = document.querySelector(".board");
+
+//   fetch("https://lanciweb.github.io/demo/api/pictures/")
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       board.innerHTML = "";
+
+//       data.forEach((item) => {
+//         const photoDiv = document.createElement("div");
+//         photoDiv.classList.add("photo");
+
+//         const img = document.createElement("img");
+//         img.src = item.url;
+//         img.alt = item.title;
+
+//         const caption = document.createElement("p");
+//         caption.classList.add("caption");
+//         caption.textContent = `${item.title} - ${item.date}`;
+
+//         photoDiv.appendChild(img);
+//         photoDiv.appendChild(caption);
+//         board.appendChild(photoDiv);
+//       });
+//     })
+//     .catch((error) => {});
+// });
